c57c222c27f8be95da5f3133766c71bbffb40c4d
Rohit <rohitsahu728@gmail.com>
2021-06-05 19:31:32 +0530

    packages

--- server/routes/SubjectRoutes.mjs
+++ server/routes/SubjectRoutes.mjs
@@ -34,6 +34,7 @@ const upload = multer({
 })
 
 router.get('/all_subjects', GetSubjects)
+router.get('/active_subjects', GetSubjects)
 router.post('/add_subject', upload.single('subject_file'), adminLoginRequired, adminProfile, AddSubject)
 router.post('/update_subject/:subjectId', upload.single('subject_file'), UpdateSubject)
 router.get('/get_subject/:subjectId', GetSubjectById)
